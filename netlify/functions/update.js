d1b1e0d82311a82f4bd4f7e3144c2387adf7f3a0
Uvaron <philippe.uvarov@gmail.com>
2025-07-09 01:10:06 +0200

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,11 +1,17 @@
-const axios = require("axios");
+const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
+  const { message } = JSON.parse(event.body);
+  await sendMessage(message.chat.id, "I got your message!");
   console.log("Received an update from Telegram!", event.body);
 
+
+
+
+
+  
   // Generate a random number between 1 and 5
   const randomNumber = Math.floor(Math.random() * 5) + 1;
-
   // Check if the random number is 1
   if (randomNumber === 1) {
     await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
