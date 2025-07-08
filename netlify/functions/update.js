ca6fbb8c3a6fc98830965615afcddce1c69f845b
Uvaron <philippe.uvarov@gmail.com>
2025-07-08 20:15:35 +0200

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
   console.log("Received an update from Telegram!", event.body);
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
   chat_id: JSON.parse(event.body).message.chat.id,
-  text: "Добрый день! Я ваш персональный шлюхабот",
+  text: "",
 });  
   return { statusCode: 200 };
 };
