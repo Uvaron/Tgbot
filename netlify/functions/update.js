af73724764a5129d088c2b11efb770bee464c05d
Uvaron <philippe.uvarov@gmail.com>
2025-07-13 16:10:19 +0200

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,8 +2,8 @@ const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
-  await sendMessage(message.chat.id, "");
-  console.log("Received an update from Telegram!", event.body);
+  await sendMessage(message.chat.id, "ниггер");
+  //console.log("Received an update from Telegram!", event.body);
 
 
 
