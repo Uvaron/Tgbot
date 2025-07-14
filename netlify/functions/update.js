78c2beaecc84fdd419dd4582a174b53f4ec7ff5d
Uvaron <philippe.uvarov@gmail.com>
2025-07-14 19:56:29 +0200

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -14,17 +14,8 @@ exports.handler = async (event) => {
 
 
   
-  // Generate a random number between 1 and 5
-  const randomNumber = Math.floor(Math.random() * 5) + 1;
-  // Check if the random number is 1
-  if (randomNumber === 1) {
-    await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-      chat_id: JSON.parse(event.body).message.chat.id,
-      text: "ты чу",
-    });
-  } else {
-    console.log("Random number is not 1. No message sent.");
-  }
+  
+  
 
   return { statusCode: 200 };
 };
