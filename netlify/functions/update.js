aa62f4e36a0991447f8f7473ae040ba99aa307a0
Uvaron <philippe.uvarov@gmail.com>
2025-07-14 16:41:00 +0200

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,8 +2,12 @@ const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
+  // Generate a random number between 1 and 5
+  const randomNumber = Math.floor(Math.random() * 5) + 1;
+  // Check if the random number is 1
+  if (randomNumber === 1) {
   await sendMessage(message.chat.id, "");
-  console.log("Received an update from Telegram!", event.body);
+  console.log("Received an update from Telegram!", event.body);}
 
 
 
