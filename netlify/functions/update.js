dbbd25767ec4a446f32f6d4797b981868ddfe52d
Uvaron <philippe.uvarov@gmail.com>
2025-07-18 20:55:09 +0200

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,23 +1,20 @@
 const sendMessage = require("../../sendMessage");
 
 exports.handler = async (event) => {
-  const { message } = JSON.parse(event.body);
-  // Generate a random number between 1 and 5
-  const randomNumber = Math.floor(Math.random() * 5) + 1;
-  // Check if the random number is 1
-  if (randomNumber === 1) {
-  await sendMessage(message.chat.id, "че ты лысый");
-  console.log("Received an update from Telegram!", event.body);}
-
-
-
-
-
-  
-  
-  
-
-  return { statusCode: 200 };
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  try {
+    const { message } = JSON.parse(event.body);
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    if (randomNumber === 1) {
+      await sendMessage(message.chat.id, "че ты лысый");
+      console.log("Received an update from Telegram!", event.body);
+    }
+    return { statusCode: 200, body: "OK" };
+  } catch (err) {
+    return { statusCode: 400, body: "Invalid JSON or missing message" };
+  }
 };
 
 
