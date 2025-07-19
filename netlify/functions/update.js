39bfa2ba0b17c649171117a801cccf368955038a
Uvaron <philippe.uvarov@gmail.com>
2025-07-19 18:50:53 +0200

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -24,7 +24,7 @@ exports.handler = async (event) => {
   const { message } = JSON.parse(event.body);
   const { botName, command, extra } = messageParts(message.text);
 
-  if (botName === "Queens_never_cry_bot" || botName === "") {
+  if (botName === "Queens_never_cry_bot" || botName === null) {
     switch (command) {
   case "Queens":
     const randomIndex = Math.floor(Math.random() * queens.length);
