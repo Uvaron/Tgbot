1e8a7bc1eeb99364752283493eafaa884f724664
Uvaron <philippe.uvarov@gmail.com>
2025-07-21 16:30:43 +0200

    Update messageParts.js

--- messageParts.js
+++ messageParts.js
@@ -1,12 +1,16 @@
 module.exports = (text) => {
-  const commandMatch = text.match(/(?<=\/).*?(?=$| |@)/);
+  if (!text) {
+    return { command: null, botName: null, extra: null };
+  }
+
+  const commandMatch = text.match(/(?<=\/)\w+/);
   const command = commandMatch ? commandMatch[0] : null;
 
-  const botNameMatch = text.match(/(?<=@).*?(?=($| ))/);
+  const botNameMatch = text.match(/(?<=@)\w+/);
   const botName = botNameMatch ? botNameMatch[0] : null;
 
-  const extraMatch = text.match(/(?<=\s).*?(?=$)/);
-  const extra = extraMatch ? extraMatch[0] : null;
+  const extraMatch = text.match(/(?<=\s).+/);
+  const extra = extraMatch ? extraMatch[0].trim() : null;
 
   return {
     command,
