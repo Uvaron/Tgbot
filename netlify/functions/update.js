f5b3e281f55a235ec16652df4e462910f69cde39
Uvaron <philippe.uvarov@gmail.com>
2025-07-22 20:15:55 +0200

    Update update.js

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -44,6 +44,14 @@ exports.handler = async (event) => {
           await sendMessage(message.chat.id, "Дарова, я Королевабот, напиши мне любую команду из того, что я сейчас перечислю и мы начнём!");
           await sendMessage(message.chat.id, "/Qecho @Q - для эха, /Queens @Q - узнай, какой страны ты Королева");
           break;
+         case "duel":
+          if (!extra) {
+            await sendMessage(message.chat.id, "Пожалуйста, укажите пользователя для дуэли.");
+            break;
+          }
+          const duelTarget = extra.trim(); // Получаем имя пользователя
+          await sendMessage(message.chat.id, `Привет, ${duelTarget}, ты вызван на дуэль!`);
+          break;
          
 
 
